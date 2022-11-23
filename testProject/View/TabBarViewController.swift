156d12b9e42fda20cbd518641be2b6b29511f0dd
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-23 14:53:07 +0900

    [Feat.] 화면 구성을 위한 TabBarViewController 생성

---
+++ testProject/View/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.swift
+//  testProject
+//
+//  Created by YongJin on 2022/11/23.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
