ff38875347bff812b3e6489dab150c80eb707fbc
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-23 17:27:34 +0900

    [Feat.] 좋아요(찜) 화면 WishListViewController 추가

---
+++ testProject/View/WishListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WishListViewController.swift
+//  testProject
+//
+//  Created by YongJin on 2022/11/23.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
