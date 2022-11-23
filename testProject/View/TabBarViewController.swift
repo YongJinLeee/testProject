a6146b3288ddcc5b2fc9ed36099f0a8a62741f2e
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-23 18:40:22 +0900

    [Feat.] 각 아이템의 NavigationViewController, TabBarItem 관련 설정
    - defalutTabIndex 인덱스가 각각의 아이템 선택 될 때마다 해당 index값으로 설정되도록 didSet 구현
    - 각 TabBarItem 이미지, 타이틀 설정

--- testProject/View/TabBarViewController.swift
+++ testProject/View/TabBarViewController.swift
@@ -8,10 +8,52 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
+    
+    var defalutTabIndex = 0 {
+        
+        didSet {
+            self.selectedIndex = defalutTabIndex
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        self.view.backgroundColor = .white
+        self.selectedIndex = defalutTabIndex
+         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 홈 탭
+        let homeNavigationController = UINavigationController()
+        let homeViewContoller = HomeViewController()
+        
+        homeNavigationController.addChild(homeViewContoller)
+        homeNavigationController.tabBarItem.title = "홈"
+        homeNavigationController.tabBarItem.image = UIImage.init(systemName: "house")
+        homeNavigationController.tabBarItem.selectedImage = UIImage.init(systemName: "house.fill")
+        homeNavigationController.tabBarItem.selectedImage?.withTintColor(UIColor.red)
+        
+        // 좋아요 탭
+        let wishListNavigationController = UINavigationController()
+        let wishListViewController = WishListViewController()
+        
+        wishListNavigationController.addChild(wishListViewController)
+        wishListNavigationController.tabBarItem.title = "좋아요"
+        wishListNavigationController.tabBarItem.image = UIImage.init(systemName: "heart")
+        wishListNavigationController.tabBarItem.selectedImage = UIImage.init(systemName: "heart.fill")
+        wishListNavigationController.tabBarItem.selectedImage?.withTintColor(UIColor.red)
+        
+        let viewControllers = [homeNavigationController, wishListNavigationController] // array index 대로 탭 표시됨
+        self.setViewControllers(viewControllers, animated: true)
+        
+    }
+}
+
+
+extension TabBarViewController {
+    
 }
