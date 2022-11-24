9246a06e29a1e4897ae099fa40c65764b3b376b1
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-24 16:06:55 +0900

    [Feat.] Moya TargetType 설정
    - 사용할 API (initialization, pagination ) case 생성
    - baseURL, case별 path, task 설정
    - method는 get방식을 사용하기 때문에 별도 설정없이 defalut만 .get 으로 설정

--- testProject/Moya/ProjectAPI.swift
+++ testProject/Moya/ProjectAPI.swift
@@ -7,3 +7,52 @@
 
 import Foundation
 import Moya
+
+public enum ProjectAPI {
+    
+    case initialization
+    case pagination(params: [String: Any])
+}
+
+extension ProjectAPI: TargetType {
+    public var baseURL: URL {
+        
+        guard let url = URL(string:"http://d2bab9i9pr8lds.cloudfront.net" ) else {
+            fatalError("baseURL could not be configured.")
+        }
+        return url
+    }
+    
+    public var path: String {
+        switch self {
+            
+        case .initialization:
+            return "/api/home"
+        case .pagination:
+            return "/api/home/goods"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+            
+        default:
+            return .get
+        }
+    }
+    
+    public var task: Moya.Task {
+        switch self {
+            
+        case .initialization:
+            return .requestPlain
+        case let .pagination(params):
+            return .requestParameters(parameters: params, encoding: URLEncoding.default)
+        }
+    }
+    
+    public var headers: [String : String]? {
+        return NetworkAPI.headers
+    }
+    
+}
