328e50a4ba2bac8a396b2fa65382f9a9db85b171
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-24 16:27:38 +0900

    [Feat.] response data 처리할 Model 구현
    - 데이터 통신시 응답값 유실 가능성때문에 응답값이 배열 일경우 옵셔널 사용

---
+++ testProject/Model/HomeModel.swift
@@ -0,0 +1,41 @@
+//
+//  HomeModel.swift
+//  testProject
+//
+//  Created by YongJin on 2022/11/24.
+//
+
+import Foundation
+
+// MARK: - Initializing
+struct Initializing: Codable {
+    let banners: [Banner]?
+    let goods: [Good]?
+}
+
+struct Pagination: Codable {
+    let goods: [Good]?
+}
+
+// MARK: - Banner
+struct Banner: Codable {
+    let id: Int
+    let image: String
+}
+
+// MARK: - Good
+struct Good: Codable {
+    let id: Int
+    let name: String
+    let image: String
+    let isNew: Bool
+    let sellCount, actualPrice, price: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, image
+        case isNew = "is_new"
+        case sellCount = "sell_count"
+        case actualPrice = "actual_price"
+        case price
+    }
+}
