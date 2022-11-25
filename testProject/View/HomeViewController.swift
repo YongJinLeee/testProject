45c78ea5bbefc582bee1a6648f9483dfa0b4b82b
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-25 17:50:13 +0900

    [Feat.] 기본 CollectionView 생성 및 Delegate, dataSource 연결

--- testProject/View/HomeViewController.swift
+++ testProject/View/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 import RxSwift
 import RxCocoa
 import SDWebImage
@@ -14,12 +15,74 @@ import SnapKit
 import Then
 
 class HomeViewController: UIViewController {
+    
+    // 상단 배너
+    var bannerCollectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout().then {
+            
+            $0.minimumLineSpacing = 0
+            $0.scrollDirection = .horizontal
+            $0.sectionInset = .zero
+        }
+        
+        let bannerView = UICollectionView(frame: .zero, collectionViewLayout: layout).then {
+            $0.backgroundColor = UIColor.systemGray
+        }
+        return bannerView
+    }()
+    
+    var goodsListCollectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout().then {
+            
+            $0.minimumLineSpacing = 0
+            $0.scrollDirection = .horizontal
+            $0.sectionInset = .zero
+        }
+        
+        let goodsListView = UICollectionView(frame: .zero, collectionViewLayout: layout).then {
+            $0.backgroundColor = UIColor.systemGray
+        }
+        return goodsListView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(bannerCollectionView)
+        view.addSubview(goodsListCollectionView)
+        
+        bannerCollectionView.delegate = self
+        bannerCollectionView.dataSource = self
+        
+        
     }
-
 
 }
 
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if collectionView == bannerCollectionView {
+            
+            return 3
+        } else {
+            
+            return 10
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+    }
+    
+    
+    
+    
+}
