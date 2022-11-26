c98dd28729107d7db5ad9cbfd0a8c1b06cb13347
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-26 17:27:45 +0900

    [Feat.] 배너, 리스트 셀  구성요소 일부 구현
    - 배너 : 배너가 들어갈 ImageView, 아이템 갯수가 표시될 Label 구성, SnapKit으로 레이아웃 설정
    - 리스트 : ImageView, Button 우선 구현. 버튼 토글 적용

--- testProject/View/GoodsListCell.swift
+++ testProject/View/GoodsListCell.swift
@@ -7,8 +7,25 @@
 
 import UIKit
 import Foundation
+import SnapKit
+import Then
+import SDWebImage
+
 
 class GoodsListCell: UICollectionViewCell {
     
-    
+    let imageView = UIImageView()
+    let wishBtn = UIButton().then {
+        
+        $0.isSelected = !$0.isSelected
+        if $0.isSelected {
+            $0.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            $0.tintColor = UIColor.systemRed
+            
+        } else {
+            $0.setImage(UIImage(systemName: "heart"), for: .normal)
+            $0.tintColor = UIColor.systemGray
+        }
+
+    }
 }
--- testProject/View/bannerCell.swift
+++ testProject/View/bannerCell.swift
@@ -7,9 +7,42 @@
 
 import UIKit
 import Foundation
+import SnapKit
+import Then
 
 class bannerCell: UICollectionViewCell {
     
+    let imageView = UIImageView()
+    let pageLabel = UILabel().then { label in
+        
+        label.textColor = UIColor.white
+        label.backgroundColor = UIColor(r: 34, g: 34, b: 34, a: 0.3)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.backgroundColor = UIColor.clear
+        contentView.clipsToBounds = true
+        
+        imageView.contentMode = .scaleAspectFill
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(pageLabel)
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        pageLabel.snp.makeConstraints {
+            
+            $0.width.equalTo(20)
+            $0.height.equalTo(10)
+            $0.trailing.bottom.equalToSuperview().offset(20)
+    
+        }
+        
+    }
     
     
 }
