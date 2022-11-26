28a8ede198bbc5d462228e5a4ca256e47163d26c
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-26 23:30:51 +0900

    [Fixed] UICollectionViewCompositionalLayout 사용하여 2개의 섹션으로 나뉜 CollectionView 구현
    - HomeViewController가 UICollectionViewController를 상속
    - dataSource, delegate 등 CollectionView 구성에 필요한 프로토콜들을 extension에 구성
    - 각 섹션에 필요한 Banner, GoodsList Cell을 CollectionView에 등록
    - NSCollectionLayoutSection 함수를 통해 각 섹션의 기본 크기, 배치 위치 구성
    
    [Feat.] SwiftUI Preview 기능 추가 (화면구성 보조)

--- testProject/View/HomeViewController.swift
+++ testProject/View/HomeViewController.swift
@@ -13,76 +13,178 @@ import SDWebImage
 import Kingfisher
 import SnapKit
 import Then
+import SwiftUI
 
-class HomeViewController: UIViewController {
+//class HomeViewController: UIViewController {
+//}
+
+class HomeViewController: UICollectionViewController {
     
-    // 상단 배너
-    var bannerCollectionView: UICollectionView = {
-        
-        let layout = UICollectionViewFlowLayout().then {
-            
-            $0.minimumLineSpacing = 0
-            $0.scrollDirection = .horizontal
-            $0.sectionInset = .zero
-        }
-        
-        let bannerView = UICollectionView(frame: .zero, collectionViewLayout: layout).then {
-            $0.backgroundColor = UIColor.systemGray
-        }
-        return bannerView
-    }()
-    
-    var goodsListCollectionView: UICollectionView = {
-        
-        let layout = UICollectionViewFlowLayout().then {
-            
-            $0.minimumLineSpacing = 0
-            $0.scrollDirection = .horizontal
-            $0.sectionInset = .zero
-        }
-        
-        let goodsListView = UICollectionView(frame: .zero, collectionViewLayout: layout).then {
-            $0.backgroundColor = UIColor.systemGray
-        }
-        return goodsListView
-    }()
-    
+    var initData: [Initializing] = []
+    var bannerData: [Banner] = []
+    var goodsData: [Good] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(bannerCollectionView)
-        view.addSubview(goodsListCollectionView)
-        
-        bannerCollectionView.delegate = self
-        bannerCollectionView.dataSource = self
-        
-        
+
+        setNaviBar()
+        getInitData()
+    
+        collectionView.register(bannerCell.self, forCellWithReuseIdentifier: "bannerCell")
+        collectionView.register(GoodsListCell.self, forCellWithReuseIdentifier: "GoodsListCell")
+
+        collectionView.collectionViewLayout = layout()
+
     }
 
 }
 
 
-extension HomeViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        if collectionView == bannerCollectionView {
-            
-            return 3
-        } else {
-            
-            return 10
+extension HomeViewController {
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        switch section {
+
+        case 0:
+            return bannerData.count
+        default:
+            return goodsData.count
         }
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        
+
     }
     
     
+    // 섹션에 따라 보여줄 Cell 설정
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        switch indexPath.section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bannerCell", for: indexPath) as? bannerCell else { return UICollectionViewCell() }
+
+            let imageURL = self.bannerData[indexPath.item].image
+            // cell.imageView.kf.setImage(with: URL(string: imageURL)) // kingFisher 버전..
+            cell.imageView.sd_setImage(with: URL(string: imageURL))
+            cell.pageLabel.text = "\(bannerData[indexPath.item].id)/\(bannerData.count)"
+            return cell
+
+        default:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GoodsListCell", for: indexPath) as? GoodsListCell else { return UICollectionViewCell() }
+
+            return cell
+        }
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
+}
+
+// ViewModel 아닌가..?
+extension HomeViewController {
+
+    func getInitData() {
+
+        ProjectAPIManager.initialization { initDataDTO in
+
+
+            self.initData = [initDataDTO]
+            self.bannerData = initDataDTO.banners
+            self.goodsData = initDataDTO.goods
+
+        } onFailure: { (error) in
+            print("\(error)")
+        }
+
+    }
+}
+
+extension HomeViewController {
+
+    func setNaviBar() {
+
+        navigationController?.navigationBar.backgroundColor = UIColor(r: 245, g: 245, b: 245, a: 0.5)
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+
+        navigationItem.title = "홈"
+    }
+
+    // 섹션별 UICollectionViewLayout 생성
+    private func layout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { [weak self] sectionType, environment -> NSCollectionLayoutSection? in
+            
+            guard let self = self else { return nil }
+
+            return self.creatBannerTypeSection()
+        }
+    }
+
+    // 배너 layoutSection
+    private func creatBannerTypeSection() -> NSCollectionLayoutSection {
+
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.9))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(300))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .paging
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+
+        return section
+
+    }
     
-    
+    private func creatGoodsListTypeSection() -> NSCollectionLayoutSection {
+
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(300))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(500))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .paging
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+
+        return section
+
+    }
+
+}
+
+
+
+// swiftUI preview
+struct HomeViewController_Previews: PreviewProvider {
+
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            let layout = UICollectionViewLayout()
+            let homeViewController = HomeViewController(collectionViewLayout: layout)
+            return UINavigationController(rootViewController: homeViewController)
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        }
+
+        typealias UIViewControllerType = UIViewController
+    }
 }
