068ed55c104c1a252a3427d84c00d35bf4b887c6
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-26 23:18:30 +0900

    [Fixed] 상품 카드 Cell 구성에 필요한 UI 요소 추가

--- testProject/View/GoodsListCell.swift
+++ testProject/View/GoodsListCell.swift
@@ -20,7 +20,7 @@ class GoodsListCell: UICollectionViewCell {
         $0.isSelected = !$0.isSelected
         if $0.isSelected {
             $0.setImage(UIImage(systemName: "heart.fill"), for: .normal)
-            $0.tintColor = UIColor.systemRed
+            $0.tintColor = UIColor(r: 236, g: 94, b: 101)
             
         } else {
             $0.setImage(UIImage(systemName: "heart"), for: .normal)
@@ -28,4 +28,26 @@ class GoodsListCell: UICollectionViewCell {
         }
 
     }
+    
+    // FIXME: 라벨 하나로 사용하고 attribute에서 할인율 부분만 컬러 적용
+    let discountLateLbl = UILabel().then {
+        
+        $0.textColor = UIColor(r: 236, g: 94, b: 101)
+    }
+    
+    let priceLbl = UILabel().then {
+        
+        $0.textColor = UIColor.black
+    }
+    let nameLbl = UILabel().then {
+        
+        $0.textColor = UIColor(r: 119, g: 119, b: 119)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+    }
+    
 }
