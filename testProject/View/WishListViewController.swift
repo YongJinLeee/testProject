59ebf50c7534113f68d46cc7a3c30e5ce656ba42
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-11-26 17:24:07 +0900

    [Commentary] 찜목록 구현 필요 요건 요약 코멘터리 작성

--- testProject/View/WishListViewController.swift
+++ testProject/View/WishListViewController.swift
@@ -12,6 +12,9 @@ class WishListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        /// Todo: HomeVeiwController에서 좋아요 눌린 아이템들을 가져와야함
+        /// 1. 데이터 담긴 별도 배열 운용
+        /// 2. 최근에 눌린 것부터 정렬해야하므로 배열에 데이터 추가할 때는 addend first
+        /// 3. UserDefalut 사용해서 종료시에도 데이터 유지되도록..?
     }
 }
